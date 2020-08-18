853d315465d6471ec789b67880e8ff70736c269f
EunSuKim <eunsu.kim3761@gmail.com>
2020-08-18 14:47:53 +0900

    menu_detail.htmll 추가

--- order/models.py
+++ order/models.py
@@ -1,23 +1,50 @@
 from django.db import models
 
 
+#coffee,desserts,goods 모델 추가 김은수
 class Coffee(models.Model):
+    text = models.CharField(max_length=100)
     cd = models.IntegerField()
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=20)
+    allergy = models.CharField(max_length=100, blank=True, null=True)
     kcal = models.IntegerField()
-    sat_fat = models.IntegerField(db_column='sat_FAT')
+    sat_fat = models.IntegerField(db_column='sat_FAT')  # Field name made lowercase.
     protein = models.IntegerField()
     fat = models.IntegerField()
-    trans_fat = models.IntegerField(db_column='trans_FAT')
+    trans_fat = models.IntegerField(db_column='trans_FAT')  # Field name made lowercase.
     sodium = models.IntegerField()
     sugars = models.IntegerField()
-    caffeine = models.IntegerField()
     cholesterol = models.IntegerField()
     chabo = models.IntegerField()
+    price = models.IntegerField()
 
     class Meta:
         managed = False
         db_table = 'Coffee'
 
-    def __str__(self):
-        return self.name
+
+
+class Desserts(models.Model):
+    text = models.CharField(max_length=100)
+    cd = models.IntegerField()
+    name = models.CharField(max_length=20)
+    gram = models.IntegerField()
+    allergy = models.CharField(max_length=100, blank=True, null=True)
+    price = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'Desserts'
+
+
+
+class Goods(models.Model):
+    text = models.CharField(max_length=100)
+    cd = models.IntegerField()
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'Goods'
+
--- order/views.py
+++ order/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from order.models import Coffee
+from order.models import Coffee,Desserts, Goods
 
 # Create your views here.
 
-def menu(request):
+# menu 함수 작성 김은수
+def menu(request): 
     if request == "POST":
         redirect("menu.html")
     else:
         coffee = Coffee.objects.all()
-        context = {"coffee" : coffee}
+        desserts = Desserts.objects.all()
+        goods = Goods.objects.all()
+        context = {"coffee" : coffee, "desserts" : desserts, "goods" : goods}
         return render(request, 'menu.html',context)
 
 

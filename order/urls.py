1c01de7801c13404625d2fb36d56116886d64fd0
EunSuKim <eunsu.kim3761@gmail.com>
2020-08-18 10:57:36 +0900

    menu.html 경로 설정

--- order/urls.py
+++ order/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
+from order import views
 
 app_name = 'order'
 urlpatterns = [ #김은수
--- order/views.py
+++ order/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from order.models import Coffee
 
 # Create your views here.
+
+
+def menu(request):
+    if request == "POST":
+        redirect("menu.html")
+    else:
+        coffee = Coffee.objects.all()
+        context = {"coffee" : coffee}
+        return render(request, 'menu.html',context)
+
 

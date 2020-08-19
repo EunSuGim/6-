420faa1b1a856fe4346e66fbf10980b5834b2c61
KeunungPark <pku928@naver.com>
2020-08-19 16:49:38 +0900

    마일리지 충전시스템 추가 ver1.4

--- membership/urls.py
+++ membership/urls.py
@@ -5,7 +5,6 @@ app_name = "membership"
 
 urlpatterns = [
     path('<int:user_n>/information/', views.information, name="information"),
-    path('<int:user_n>/information/history/', views.history, name="history"),
     path("<int:user_n>/information/recharge/", views.recharge, name="recharge"),
-
+    path('<int:user_n>/information/history/', views.history, name="history"),
 ]
--- membership/views.py
+++ membership/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect,HttpResponseRedirect,reverse
 from membership.models import Gift_card
 from accounts.models import User
 
@@ -7,11 +7,20 @@ def information(request,user_n) :
     context ={"user" : user}
     return render(request,"information.html",context)
 
+def recharge(request, user_n) :
+    user = get_object_or_404(User, id=user_n)
+    giftcard_list = Gift_card.objects.all()
+    get_number =  int( request.POST["serial"] )
+    for k in range(0,len(giftcard_list)):
+        n = giftcard_list[k].serial_number
+        if n == get_number:
+            user.point += giftcard_list[k].value
+            user.save()
+            break
+        # return HttpResponseRedirect(reverse('membership:information', args=(user_n,)))
+    return redirect("membership:information", user_n)
 
 def history(request) :
     return render(request, "history.html",{"history":[]})
 
 
-def recharge(request) :
-    return render(request, "recharge.html",{"mileage":[]})
-     # redirect로 바꿔야함

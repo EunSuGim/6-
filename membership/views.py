c5d3028c9280a025f2986f45eb418766c878736a
KeunungPark <pku928@naver.com>
2020-08-21 17:38:43 +0900

    병합전

--- membership/views.py
+++ membership/views.py
@@ -10,16 +10,19 @@ def information(request,user_n) :
 def recharge(request, user_n) :
     user = get_object_or_404(User, id=user_n)
     giftcard_list = Gift_card.objects.all()
-    get_number =  int( request.POST["serial"] )
-    for k in range(0,len(giftcard_list)):
-        n = giftcard_list[k].serial_number
-        if n == get_number:
-            user.point += giftcard_list[k].value
-            user.save()
-            break
-        # return HttpResponseRedirect(reverse('membership:information', args=(user_n,)))
-    return redirect("membership:information", user_n)
+    serial_list = [giftcard.serial_number for giftcard in giftcard_list]
 
+    get_number =  int( request.POST["serial"] )
+    if get_number in serial_list:
+        for k in range(0,len(giftcard_list)):
+            n = serial_list[k]
+            if n == get_number:
+                user.point += giftcard_list[k].value
+                user.save()
+                break
+        return redirect("membership:information", user_n)
+    else:
+        redirect
 def history(request) :
     return render(request, "history.html",{"history":[]})
 

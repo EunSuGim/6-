7e40a9df35695286a165bb9a6ade9f4776350915
EunSuGim <eunsu.kim3761@gmail.com>
2020-08-26 23:10:02 +0900

    주소 검색창 수정중

--- order/models.py
+++ order/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from accounts.models import User
 
 
-#coffee,desserts,goods 모델 추가 김은수
+# coffee,desserts,goods 모델 추가 김은수
 class Coffee(models.Model):
     text = models.CharField(max_length=100)
     cd = models.IntegerField()
@@ -56,15 +56,24 @@ class Goods(models.Model):
     def __str__(self):
         return self.name
 
-class Carts(models.Model):
-    identity = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
-    name = models.CharField('상품명',max_length=20,null=True)
-    price = models.IntegerField('가격',default=0)
-    quantity = models.IntegerField('수량',default=0)
-    total = models.IntegerField('총가격',default=0)
-    cd = models.IntegerField('cd',null=True)
-    category = models.CharField('카테고리',max_length=20,null=True)
 
+class Carts(models.Model):
+    identity = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField('상품명', max_length=20, null=True)
+    price = models.IntegerField('가격', default=0)
+    quantity = models.IntegerField('수량', default=0)
+    total = models.IntegerField('총가격', default=0)
+    cd = models.IntegerField('cd', null=True)
+    category = models.CharField('카테고리', max_length=20, null=True)
 
     def __str__(self):
         return self.name
+
+
+class StarbucksAddress(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'starbucks_address'

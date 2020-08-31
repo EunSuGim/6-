c43257b811bd3d5ddd9f526b4d2e581ad7c68444
EunSuGim <eunsu.kim3761@gmail.com>
2020-08-31 23:54:47 +0900

    membership history함수에 user context 추가

--- membership/views.py
+++ membership/views.py
@@ -45,7 +45,7 @@ def history(request) :
 def history(request, user_n):
     user = get_object_or_404(User, id=user_n)
     user_histories = user.history_set.all().order_by('-id')
-    return render(request, "history.html",{"histories": user_histories})
+    return render(request, "history.html",{"histories": user_histories, 'user' : user}) #김은수 : context에 user 추가
 
 
 # def r_create(request, history_id):

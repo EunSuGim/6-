8394c9b689132fa37e0ddc8968a66f79a0776a4a
Keunung Park <pku928@naver.com>
2020-09-03 03:43:19 +0900

    index 리뷰수정

--- starbucks/views.py
+++ starbucks/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from membership.models import Review
-
+from membership.models import Review, History
+from accounts.models import User
 def give_reviews(request) :
     reviews = Review.objects.all().order_by('-id')
-    reviews5 = reviews[0:5]
+    my_list2 = []
+    my_list5 = []
+    j=0
+    for review in reviews :
+        user = get_object_or_404(User, id = review.user_id)
+        history = get_object_or_404(History, id = review.history_id)
+        temp= (review, user, history)
+        my_list5.append(temp)
+        if j < 2:
+            my_list2.append(temp)
 
-    return render(request, "index.html",{"reviews": reviews5})
+        if j == 4:
+            break
+        j+=1
+    return render(request, "index.html", {"my_list2": my_list2, "my_list5": my_list5})
+
+# def give_footer(request) :
+#     reviews = Review.objects.all().order_by('-id')
+#     my_list2 = []
+#     j=0
+#     for review in reviews :
+#         user = get_object_or_404(User, id = review.user_id)
+#         history = get_object_or_404(History, id = review.history_id)
+#         temp= (review, user, history)
+#         my_list2.append(temp)
+#         j += 1
+#         if j == 2:
+#             break
+#     return render(request, "index.html", {"my_list2": my_list2})
